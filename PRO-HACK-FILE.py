abc1abf1bc085c1a64d2c20e9e391398509f11ee
FLAME-CYBER-404 <102447853+FLAME-CYBER-404@users.noreply.github.com>
2022-05-14 08:54:13 +0600

    Update PRO-HACK-FILE.py

--- PRO-HACK-FILE.py
+++ PRO-HACK-FILE.py
@@ -1,14 +1,5 @@
-import os, platform
+import os, sys
 try:
-    import requests
-except:
-    os.system('pip install requests')
-os.system('git pull')
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from Brute import .Main()
-    Main()
-elif bit == '32bit':
-    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
- 
+    __import__("Brute").Main()
+except Exception as e:
+    exit(str(e))
